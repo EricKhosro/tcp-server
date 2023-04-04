62d4950a78b14927486a0649665cbbf6d8b65601
Eric Khosrafian <erickhosrafian@gmail.com>
2023-04-04 15:11:17 +0430

    added interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,23 @@
+export interface IPacket<T> {
+  msg: string;
+  errorCode?: number;
+  statusCode: number;
+  data: T;
+}
+
+export interface IMethodClass<T> {
+  methodHandler: (data: T) => IPacket<boolean>;
+}
+
+export interface IMethodFactory<T> {
+  createClass: (className: string) => IMethodClass<T> | null;
+}
+
+export interface IRequestBody {
+  id: number;
+}
+
+export interface ILoginData extends IRequestBody {
+  username: string;
+  password: string;
+}
